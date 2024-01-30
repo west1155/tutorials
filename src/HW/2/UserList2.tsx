8edd8f55b1110ec7ca11a9936f2afb9b76d86a34
Aleksandr Leimberg <aleimberg@west.fritz.box>
2024-01-30 17:31:26 +0000

    HW2

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -8,14 +8,9 @@ type UserList2PropsType = {
 
 export const UserList2 = (props: UserList2PropsType) => {
 
-
-	const sayHello = () => {
-		console.log('Hello!');
-	}
 	return (
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
-			<button onClick={sayHello}>dfsdgd</button>
 			<button id={'hw02-filter-button'} onClick={props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
 			<ul>
 			{props.users.myFriends.map((user: UserType) => (
